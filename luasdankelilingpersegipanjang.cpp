4f6d642ffb37fbfd96d46615f86bd2c1866565e6
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-03-02 12:22:06 +0700

    mencari luas dengan mengalikan variabel

--- luasdankelilingpersegipanjang.cpp
+++ luasdankelilingpersegipanjang.cpp
@@ -5,8 +5,14 @@ int panjang, lebar;
 
 void input()
 {
-     cout << "Masukkan Panjang :";
+    cout << "Masukkan Panjang :";
     cin >> panjang;
     cout << "Masukkan Lebar :";
     cin >> lebar;
 }
+
+int luasPersegi()
+{
+    return panjang * lebar;
+}
+
